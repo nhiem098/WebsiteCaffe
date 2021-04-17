77fdd64daeedd3d10b44bdc94fe8dd1d4bd90dd7
nhiem098 <nhiem098@gmail.com>
2021-04-17 10:07:06 +0700

    turn off route web

--- routes/web.php
+++ routes/web.php
@@ -16,32 +16,32 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::group(['prefix' => '/', 'middleware' => 'auth'],function () {
-    Route::get('/', function () {
-        return view('backend.dashboard');
-    })->name('home');
-    Route::get('/home', function () {
-        return view('backend.dashboard');
-    })->name('home');
+//Route::group(['prefix' => '/', 'middleware' => 'auth'],function () {
+    // Route::get('/', function () {
+    //     return view('backend.dashboard');
+    // })->name('home');
+    // Route::get('/home', function () {
+    //     return view('backend.dashboard');
+    // })->name('home');
 
-    Route::get('/logout', function () {
-        Auth::logout();
-        return redirect('/');
-    })->name('logout');
+    // Route::get('/logout', function () {
+    //     Auth::logout();
+    //     return redirect('/');
+    // })->name('logout');
 
-    // Category
-    Route::resource('category', 'CategoryController')->except(['show', 'update', 'delete']);
-    Route::prefix('category')->group(function () {
-        Route::post('update-category/{id}', 'CategoryController@update')->name('category.update');
-        Route::get('delete-category/{category}', 'CategoryController@destroy')->name('category.destroy');
-    });
+    // // Category
+    // Route::resource('category', 'CategoryController')->except(['show', 'update', 'delete']);
+    // Route::prefix('category')->group(function () {
+    //     Route::post('update-category/{id}', 'CategoryController@update')->name('category.update');
+    //     Route::get('delete-category/{category}', 'CategoryController@destroy')->name('category.destroy');
+    // });
 
-    // Product
-    Route::resource('product', 'ProductController')->except(['update', 'delete']);
-    Route::prefix('product')->group(function () {
-        Route::post('update-product/{product}', 'ProductController@update')->name('product.update');
-        Route::get('delete-product/{product}', 'ProductController@destroy')->name('product.destroy');
-    });
-});
+    // // Product
+    // Route::resource('product', 'ProductController')->except(['update', 'delete']);
+    // Route::prefix('product')->group(function () {
+    //     Route::post('update-product/{product}', 'ProductController@update')->name('product.update');
+    //     Route::get('delete-product/{product}', 'ProductController@destroy')->name('product.destroy');
+    // });
+//});
 
-Auth::routes();
+// Auth::routes();
